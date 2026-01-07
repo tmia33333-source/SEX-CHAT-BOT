88169ecd47ce77474ea64eee915abc44a638ef41
tmia33333-source <tmia33333@gmail.com>
2026-01-07 23:57:02 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -2,8 +2,8 @@ module.exports.config = {
  name: "emoji_voice",
  version: "10.0",
  hasPermssion: 0,
- credits: "𝗦𝗵𝗮𝗵𝗮𝗱𝗮𝘁 𝗦𝗔𝗛𝗨",
- description: "Emoji দিলে কিউট মেয়ের ভয়েস পাঠাবে 😍",
+ credits: "বৃষ্টি",
+ description: "Emoji দিলে ভয়েস পাঠাবে 😍",
  commandCategory: "noprefix",
  usages: "😘🥰😍",
  cooldowns: 5
