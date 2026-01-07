b54a5dfc81784409e6329b10ab70a0bd940edf79
tmia33333-source <tmia33333@gmail.com>
2026-01-07 23:48:37 +0600

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -5,7 +5,7 @@ module.exports.config = {
  name: "helpall",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐒𝐀𝐇𝐔",
+ credits: "বৃষ্টি",
  description: "Displays all available commands in one page",
  commandCategory: "system",
  usages: "[No args]",
@@ -28,18 +28,18 @@ module.exports.run = async function ({ api, event }) {
 
  const finalText = `╔═══❖ 🌟 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓 🌟 ❖═══╗
 ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
-╠═════🔰 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🔰═════╣
-║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+╠═════🔰 বৃষ্টি 𝐈𝐍𝐅𝐎 🔰═════╣
+║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌বৃষ্টি
+║ 👑 𝐎𝐰𝐧𝐞𝐫: বৃষ্টি দাস 
 ║ 📦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬: ${allCommands.length} 
 ╚═══════════════════════╝`;
 
  
  const backgrounds = [
- "https://i.imgur.com/wu0iDqS.jpeg",
- "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/Huz3nAE.png",
- "https://i.imgur.com/wu0iDqS.jpeg"
+ "https://img.sanishtech.com/u/609ae28d2a5b184b3b91a70203ad2f38.webp",
+ "https://img.sanishtech.com/u/d6902e9a76c868ca254bf5c36c963757.webp",
+ "https://img.sanishtech.com/u/90fe89e143377ec42f0665dde769802d.webp",
+ "https://img.sanishtech.com/u/826a97ee7350183615958201dfb13142.jpg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
