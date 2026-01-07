50907e7c3153b110aba65272385bdffa249947bc
tmia33333-source <tmia33333@gmail.com>
2026-01-07 22:46:23 +0600

    Update antijoin.js

--- Script/events/antijoin.js
+++ Script/events/antijoin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antijoin",
  eventType: ["log:subscribe"],
  version: "1.0.0",
- credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+ credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 বৃষ্টি",
  description: "Welcome new members to the group"
 };
 
