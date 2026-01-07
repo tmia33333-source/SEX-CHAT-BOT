c2de7e27452536e079693b64548b7b2c5f97d605
tmia33333-source <tmia33333@gmail.com>
2026-01-07 22:56:35 +0600

    Update autosetname.js

--- Script/events/autosetname.js
+++ Script/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -বৃষ্টি ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Automatically set new member nicknames"
 };
 
