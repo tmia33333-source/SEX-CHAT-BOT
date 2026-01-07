29ed66504736edbe673a9308dbb06f178f5067f2
tmia33333-source <tmia33333@gmail.com>
2026-01-07 22:58:04 +0600

    Update antirobbery.js

--- Script/events/antirobbery.js
+++ Script/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -বৃষ্টি ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "Prevent admin changes",
 };
 
