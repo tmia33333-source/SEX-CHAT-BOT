b27c41cb23a118fe6667aeee8a7bd28d6ce5d894
tmia33333-source <tmia33333@gmail.com>
2026-01-08 12:06:51 +0600

    Update adminupdate.js

--- Script/events/adminupdate.js
+++ Script/events/adminupdate.js
@@ -2,7 +2,7 @@ module.exports.config = {
 name: "adminUpdate",
 eventType: ["log:thread-admins","log:thread-name", "log:user-nickname","log:thread-icon","log:thread-call","log:thread-color"],
 version: "1.0.1",
-credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 বৃষ্টি",
+credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 AI",
 description: "Update team information quickly",
 envConfig: {
 sendNoti: true,
