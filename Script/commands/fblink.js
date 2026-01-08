c34c9b23da43c7f48f150c61095ec4e124f66086
tmia33333-source <tmia33333@gmail.com>
2026-01-08 15:06:36 +0600

    Update fblink.js

--- Script/commands/fblink.js
+++ Script/commands/fblink.js
@@ -2,16 +2,16 @@ module.exports.config = {
 	name: "fblink",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Shahadat SAHU",
+	credits: "বৃষ্টি দাস",
 	description: "Get linkfb.",
 	commandCategory: "Generate FB id link",
 	cooldowns: 5
 };
 
 module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`https://www.facebook.com/profile.php?id=${event.senderID}`, event.threadID, event.messageID);
+	if (Object.keys(event.mentions) == 0) return api.sendMessage(`https://www.facebook.com/profile.php?id=100086341322391${event.senderID}`, event.threadID, event.messageID);
 	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: https://www.facebook.com/profile.php?id=${Object.keys(event.mentions)[i]}`, event.threadID);
+		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: https://www.facebook.com/profile.php?id=100086341322391${Object.keys(event.mentions)[i]}`, event.threadID);
 		return;
 	}
  }
