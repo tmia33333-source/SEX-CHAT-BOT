90342a1df67156b3b24b54fce22c464b2e3594d2
tmia33333-source <tmia33333@gmail.com>
2026-01-08 09:30:22 +0600

    Update owner2.js

--- Script/commands/owner2.js
+++ Script/commands/owner2.js
@@ -5,7 +5,7 @@ module.exports.config = {
  name: "owner2",
  version: "1.0.1",
  hasPermssion: 0,
- credits: "Shahadat SAHU",
+ credits: "বৃষ্টি দাস",
  description: "Display bot owner's information",
  commandCategory: "Info",
  usages: "",
@@ -18,7 +18,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event }) {
- const imageUrl = "https://graph.facebook.com/100001039692046/picture?height=720&width=720&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662";
+ const imageUrl = "https://www.facebook.com/100086341322391/picture?height=720&width=720&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662";
  const path = __dirname + "/cache/owner.png";
 
  request(imageUrl)
@@ -28,9 +28,9 @@ module.exports.run = async function ({ api, event }) {
  body:
 `🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟
 
-👑 𝗡𝗮𝗺𝗲: Shahadat Islam😘
-😻 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: মেয়েদের মনে🙈
-💼 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻: মেয়েদের মন জয় করা😍
+👑 𝗡𝗮𝗺𝗲: বৃষ্টি দাস 😘
+😻 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: ছেলে দের মনে🙈
+💼 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻: ছেলে দের মন জয় করা😍
 
 🌐 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: আইডি বেইচ্চা খাইয়া লাইছি😁
 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿: দিলে Future বউ ধইরা মারব😌
@@ -40,7 +40,7 @@ module.exports.run = async function ({ api, event }) {
 🎵 𝗧𝗶𝗸𝗧𝗼𝗸: সরি আমি প্রতিবন্ধী না🥱
 👻 𝗦𝗻𝗮𝗽𝗰𝗵𝗮𝘁: তোদের মতো কালা নাকি ফিল্টার লাগামু🤭
 
-🤖 𝗕𝗢𝗧 𝗕𝗬: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+🤖 𝗕𝗢𝗧 𝗕𝗬: ─꯭─⃝‌‌বৃষ্টি দাস
 `,
  attachment: fs.createReadStream(path)
  }, event.threadID, () => fs.unlinkSync(path));
