699043a89d3d0cfe30ced55377c3d0a7f2ebd3df
tmia33333-source <tmia33333@gmail.com>
2026-01-08 09:58:35 +0600

    Update caption.js

--- Script/commands/caption.js
+++ Script/commands/caption.js
@@ -4,7 +4,7 @@ module.exports.config = {
   name: "caption",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "বৃষ্টি দাস",
   description: "random caption",
   commandCategory: "caption",
   usages: "caption",
